5c981d0405b04b1a55fe909874ce903170056819
Nikita <nikitosminsk100@gmail.com>
2022-05-03 15:55:18 +0300

    LR3 almost done

--- lab03/ex4.s
+++ lab03/ex4.s
@@ -1,6 +1,7 @@
 .globl iterative
 .globl recursive
 
+
 .data
     n: .word 8
     m: .word 2
@@ -23,16 +24,18 @@
 
     tester:
         addi a1, zero, 5
+        addi a0, zero, 5
     
         sw ra, 8(sp)
-	    jal iterative
+	    #jal iterative
+        jal recursive
         addi t3, a0, 0
         lw ra, 8(sp)
 
         jr ra
 
     iterative: # a1 ---> argument
-        addi t0, zero, 0 # counter
+        mv t0, zero # counter
         addi a0, zero, 1 # response
 
         count_factorial_iterative:
@@ -45,6 +48,28 @@
         finished_counting_factorial_iterative:
             jr ra
     
-    recursive:
-        # YOUR CODE HERE
-	    #jalr zero, 0(ra)
+    recursive: #--> a0 - argument
+        addi sp, sp, -8
+
+        sw a0, 4(sp)
+        sw ra, 0(sp)
+
+        addi t0, zero, 1
+
+        bgt a0, t0, return
+
+        addi a0, zero, 1
+        addi sp, sp, 8
+        jr ra
+
+        return:
+            addi a0, a0, -1
+            jal recursive
+
+            lw t1, 4(sp)
+            lw ra, 0(sp)
+
+            addi sp, sp, 8
+            mul a0, t1, a0
+
+            jr ra
