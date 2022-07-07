6780369be08f6a9f8f1019549891c35a9ecd023f
Nikita <nikitosminsk100@gmail.com>
2022-07-07 17:33:24 +0300

    Pulled last tasks and saved reports for LR4 and LR5. Don't know, why, but just to keep all my repositories up to date

--- lab03/discrete_fn.s
+++ lab03/discrete_fn.s
@@ -1,143 +1,49 @@
-# Дирректива .globl определяет функции, которые мы хотим экспортировать
-# похоже на размещение объявления функции в заголовке в Cи
-.globl f
+.globl function
+
 
 .data
-# asciiz директива используется дял размещения строк
-# asciiz автоматически добавит нулевой символ в конец строки
-# FIXME исправьте строки, чтобы они выдавали корректные отладочные значения 
-case1:   .asciiz "f(x) should be y, and it is: "
-case2:   .asciiz "f(x) should be y, and it is: "
-case3:   .asciiz "f(x) should be y, and it is: "
-case4:   .asciiz "f(x) should be y, and it is: "
-case5:   .asciiz "f(x) should be y, and it is: "
-case6:   .asciiz "f(x) should be y, and it is: "
-case7:   .asciiz "f(x) should be y, and it is: "
+    data_array: .word   6, 61, 17, -38, 19, 42, 5, -1 
 
-# FIXME Разместите значения из вашего варианта в этом массиве 
-output: .word   1, 2, 3, 4, 5, 6, 7
 
 .text
-main:
-	######### перебор случаев, случай 1 (case1) #########
-    # загружаем адрес строки case1 в a0
-    # это послужит аргументом функции print_str
-    la a0, case1 
-    # Выводим строку по адресу case1
-    jal print_str 
-    # Загружаем первый аргумент функции f в a0
-    # FIXME Подставьте первый аргумент case1
-    li a0, -3 
-    # загружаем второй аргумент функции f в a1
-    # `output` -- это указатель на массив, который содержит возможные выходные значения f
-    la a1, output
-    # выполняем f(case1)
-    jal f     
-    # f вернет результат f(-3) в регистре a0
-    # чтобы отобразить это значение мы вызовем print_int
-    # print_int ожидает значение аргумента в регистре a0
-    # значение уже находится в a0, не требуется перемещений
-    jal print_int
-    # print a new line
-    jal print_newline
+    main:
+        addi     a2, zero, 0 # test value to check: 0
+        la       a3, data_array # copied address of data_array to a3
 
-	######### перебор случаев, случай 2 (case2) #########
-    la a0, case2
-    jal print_str
-    # FIXME Подставьте первый аргумент case1
-    li a0, -2
-    la a1, output
-    jal f                
-    jal print_int
-    jal print_newline
+        jal      ra, function
 
-	######### перебор случаев, случай 3 (case3) #########
-    la a0, case3
-    jal print_str
-    # FIXME Подставьте первый аргумент case1
-    li a0, -1
-    la a1, output
-    jal f               
-    jal print_int
-    jal print_newline
+        jal      ra, print_int
+        jal      ra, print_newline
 
-	######### перебор случаев, случай 4 (case4) #########
-    la a0, case4
-    jal print_str
-    # FIXME Подставьте первый аргумент case1
-    li a0, 0
-    la a1, output
-    jal f               
-    jal print_int
-    jal print_newline
+        #return
+        addi     a0, zero, 10
+        ecall
 
-	######### перебор случаев, случай 5 (case5) #########
-    la a0, case5
-    jal print_str
-    # FIXME Подставьте первый аргумент case1
-    li a0, 1
-    la a1, output
-    jal f                
-    jal print_int
-    jal print_newline
 
-	######### перебор случаев, случай 6 (case6) #########
-    la a0, case6
-    jal print_str
-    # FIXME Подставьте первый аргумент case1
-    li a0, 2
-    la a1, output
-    jal f               
-    jal print_int
-    jal print_newline
+    # a2 значение для которого мы хотим вычислить функцию f
+    # a3 адрес выходного ("output") массива, содержащего все допустимые варианты.
+    # a0 - function's response
+    function:
+        addi     t0, a2, 3 # t0 = a2 + 3 (shift += 3, because I have possible args: range(-3, 3, 1)
+        slli     t0, t0, 2 # t0 *= 4
+        add      t0, t0, a3 # t0 = address of arr[counter]
+        lw       t1, 0(t0) 
+        mv       a0, t1 # response = t1 (a0 = t1)
 
-	######### перебор случаев, случай 7 (case7) #########
-    la a0, case7
-    jal print_str
-    # FIXME Подставьте первый аргумент case1
-    li a0, 3
-    la a1, output
-    jal f                
-    jal print_int
-    jal print_newline
+        jr       ra # return
 
-	# передаем 10 в ecall чтобы завершить программу
-    li a0, 10
-    ecall
 
-# f принимает два аргумента:
-# a0 значение для которого мы хотим вычислить функцию f
-# a1 адрес выходного ("output") массива, содержащего все допустимые варианты.
-f:
-    # FIXME
-    # YOUR CODE GOES HERE!
+    print_int: # from a0
+        mv      a1, a0
+        addi    a0, zero, 1
+        ecall
 
-    jr ra               # Всегда вызывайте jr ra для выхода из функции!
+        jr      ra
 
-# печатает одно целое число
-# вход: a0: число на печать
-# ничего не возвращает
-print_int:
-	# to print an integer, we need to make an ecall with a0 set to 1
-    # the thing that will be printed is stored in register a1
-    # this line copies the integer to be printed into a1
-    mv a1, a0
-    # set register a0 to 1 so that the ecall will print
-    li a0, 1
-    # print the integer
-    ecall
-    # return to the calling function
-    jr    ra
 
-# печатает строку
-print_str:
-    mv a1, a0
-    li a0, 4 # tells ecall to print the string that a1 points to
-    ecall
-    jr    ra
+    print_newline:
+        addi    a1, zero, '\n'
+        addi    a0, zero, 11
+        ecall
 
-print_newline:
-    li a1, '\n'
-    li a0, 11 # tells ecall to print the character in a1
-    ecall
-    jr    ra
+        jr      ra
